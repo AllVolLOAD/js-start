fe5282a94be3e3ce186939101e63535cf0596b1e
AllVolLOAD <102479885+AllVolLOAD@users.noreply.github.com>
2023-02-09 15:54:06 +0300

    Second day

--- III.js
+++ III.js
@@ -1,1 +1,14 @@
 /* III */
+function CheezeBoard(ver = 2, hor = 8) {
+    let count = 0
+    for (let i = 1; i <= ver+1; i++) {
+        count += 1
+        if (i % 2 === 0) {
+            console.log("# ".repeat(hor))
+        }
+        console.log(" #".repeat(hor))
+    }
+
+}
+
+CheezeBoard()
