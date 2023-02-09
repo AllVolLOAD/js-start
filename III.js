11209ca8536788a5e369c511883ea1efff02cd76
allvolload <juniormiddlehead@yandex.ru>
2023-02-09 17:34:04 +0300

    second day, try 3

--- III.js
+++ III.js
@@ -1,14 +1,15 @@
 /* III */
 function CheezeBoard(ver = 2, hor = 8) {
-    let count = 0
-    for (let i = 1; i <= ver+1; i++) {
-        count += 1
+
+    for (let i = 1; i != ver+1; i++) {
         if (i % 2 === 0) {
-            console.log("# ".repeat(hor))
+            console.log("# ".repeat(hor));
+            continue;
         }
         console.log(" #".repeat(hor))
     }
 
 }
 
-CheezeBoard()
+CheezeBoard(5,5)
+
